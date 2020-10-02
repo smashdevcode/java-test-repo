b0cfd05362ea0894b6c8a3d62909a47fa7b01a9c
James Churchill <james@smashdev.com>
2020-10-02 09:06:35 -0700

    Update Main class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Here's a second line updated");
+
+        Person person = new Person("James");
     }
 }
