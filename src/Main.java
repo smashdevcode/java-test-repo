1fd76a2f6695d70bb7958c40814d6c3814ed0627
James Churchill <james@smashdev.com>
2020-10-02 09:04:11 -0700

    Update Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Here's a second line");
+        System.out.println("Here's a second line updated");
     }
 }
